f024b98e53b23e0d25f520662bab22d1a82dd258
Gcvishwas <gcvishwas17@gmail.com>
2025-01-22 22:44:53 +0545

    Initial commit

---
+++ frontend/src/routers/router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom"
+import App from "../App"
+import Home from "../pages/Home/home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/orders",
+                element: <h1>Orders</h1>
+            },
+        ]
+    },
+]);
+
+export default router
