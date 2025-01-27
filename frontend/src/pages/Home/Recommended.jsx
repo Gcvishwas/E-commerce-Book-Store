2b756d9de7fa833dcaf9f4cc40fbe01b1ba9aec0
Gcvishwas <gcvishwas17@gmail.com>
2025-01-27 22:15:23 +0545

    Recommendation components

---
+++ frontend/src/pages/Home/Recommended.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Recommended = () => {
+    return (
+        <div>
+            <h2 className="text-4xl font-bold text-center mb-8 text-gray-800">Confused? Here's what you can read</h2>
+        </div>
+    )
+}
+
+export default Recommended
