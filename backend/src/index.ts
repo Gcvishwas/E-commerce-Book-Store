9024839d362b00e29b855b340c72dfc7be37b8bb
ujjwal-207 <ujjwalnepal2060@gmail.com>
2025-04-14 19:45:11 +0545

    remove / form link in cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://e-commerce-book-store-readit.vercel.app/"],
+  origin: ["http://localhost:5173", "https://e-commerce-book-store-readit.vercel.app"],
   credentials: true,
 };
 
