af58468103d93fafde1ec81bfbcb9647acdb8c66
Gcvishwas <gcvishwas17@gmail.com>
2025-04-17 13:41:55 +0545

    URL update

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
-  return "https://e-commerce-book-store-1.onrender.com/api/books/";
+  return "https://readitbackend.onrender.com";
   // return "http://localhost:5000"; // Uncomment this line to use localhost for development
-};
+}
 
 export default getBaseUrl;
