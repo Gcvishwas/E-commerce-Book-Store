53f71fc2ebdd3141d1ec317ba2eda45aada45dce
Gcvishwas <gcvishwas17@gmail.com>
2025-04-17 13:22:23 +0545

    URL update with backend site

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-  return "https://readitbackend.onrender.com";
+  return "https://e-commerce-book-store-1.onrender.com";
   // return "http://localhost:5000"; // Uncomment this line to use localhost for development
 };
 
