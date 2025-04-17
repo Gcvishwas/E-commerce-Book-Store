84e7d4c736f7bdaf7c59a305a16901de3b41c96f
Gcvishwas <gcvishwas17@gmail.com>
2025-04-17 13:38:20 +0545

    Updated baseURL with books Route URL

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-  return "https://e-commerce-book-store-1.onrender.com";
+  return "https://e-commerce-book-store-1.onrender.com/api/books";
   // return "http://localhost:5000"; // Uncomment this line to use localhost for development
 };
 
